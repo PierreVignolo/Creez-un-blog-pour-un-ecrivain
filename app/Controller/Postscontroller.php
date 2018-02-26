6169e4fc67c28720d1b598610f04f86336473ed4
PierreVignolo <developpeurweb@pierrevignolo.fr>
2018-02-26 21:06:23 +0100

    Update de PostsController

--- app/Controller/Postscontroller.php
+++ app/Controller/Postscontroller.php
@@ -4,7 +4,7 @@ namespace App\Controller;
 
 use Core\Controller\Controller;
 
-class Postscontroller extends AppController
+class PostsController extends AppController
 {
 
     public function __construct() {
