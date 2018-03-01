c73d9de2fa87e41d0539890f0c336a2464cd9522
PierreVignolo <developpeurweb@pierrevignolo.fr>
2018-03-01 11:20:48 +0100

    rétablissement du système d'authentification

--- app/Views/users/login.php
+++ app/Views/users/login.php
@@ -1,19 +1,10 @@
-<?php
+<?php if($errors): ?>
+    <div class="alert alert-danger">
+        Identifiants Incorrects
+    </div>
+<?php endif ?>    
 
-if (!empty ($_POST)) {
-    $auth = new \Core\Auth\DBAuth(App::getInstance()->getDb());
-    if ($auth->login($_POST['username'], $_POST['password'])) {
-        header('Location: admin.php');
-    } else {
-        ?>
-        <div class="alert alert-danger">
-            Identifiants Incorrect
-        </div>
-        <?php
-    }
-}
-$form = new \Core\HTML\BootstrapForm($_POST);
-?>
+
 
 <form method="post">
     <?= $form->input('username', 'Pseudo'); ?>
