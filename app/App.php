1f80933541a94aeecc843fef497c312e6a0f317c
PierreVignolo <developpeurweb@pierrevignolo.fr>
2018-03-11 16:09:35 +0100

    update de App

--- app/App.php
+++ app/App.php
@@ -3,41 +3,36 @@
 use Core\Config;
 use Core\Database\MysqlDatabase;
 
+class App{
 
-class App 
-{
-    
-    public $title = "Mon Super Site";
+    public $title = "Mon super site";
     private $db_instance;
     private static $_instance;
 
-    public static function getInstance()
-    {
-        if (self::$_instance === null) {
+    public static function getInstance(){
+        if(is_null(self::$_instance)){
             self::$_instance = new App();
         }
         return self::$_instance;
     }
 
-    public static function load()
-    {   session_start();
+    public static function load(){
+        session_start();
         require ROOT . '/app/Autoloader.php';
         App\Autoloader::register();
         require ROOT . '/core/Autoloader.php';
         Core\Autoloader::register();
     }
 
-    public function getTable($name)
-    {
+    public function getTable($name){
         $class_name = '\\App\\Table\\' . ucfirst($name) . 'Table';
         return new $class_name($this->getDb());
     }
 
-    public function getDb()
-    {
+    public function getDb(){
         $config = Config::getInstance(ROOT . '/config/config.php');
-        if ($this->db_instance === null) {
-            $this->db_instance = new MysqlDatabase($config->get('db_name'),$config->get('db_name'), $config->get('db_user'), $config->get('db_pass'), $config->get('db_host'));
+        if(is_null($this->db_instance)){
+            $this->db_instance = new MysqlDatabase($config->get('db_name'), $config->get('db_user'), $config->get('db_pass'), $config->get('db_host'), $config->get('db_charset'));
         }
         return $this->db_instance;
     }
